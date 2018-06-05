304ac87d86fa77378b1f82d927a3254b1309b22f
matthew <mpalma@sparc.network>
2018-06-05 10:36:02 -0400

    Added comments and robustness to resubmitter.js

--- resubmitter.js
+++ resubmitter.js
@@ -1,15 +1,17 @@
-/*
- * @file resubmitter.js
- * Copyright (c) 2018, Kings Distributed Systems, Ltd.  All Rights Reserved.
+/**
+ *  @file resubmitter.js
  *
- * Simple node process that will check for pending tasks that are past thier
- * expected completion time and request that the board puts them back onto
- * the heap.
+ *  Simple node process that will check for pending tasks that are past thier
+ *  expected completion time and request that the board puts them back onto
+ *  the heap.
  *
- * Will be run from a cronjob, every X minutes.
+ *  Will be run from a cronjob, every X minutes.
  *
- * @author Matthew Palma, mpalma@sparc.network
- * @date June 2018
+ *  @author Matthew Palma, mpalma@sparc.network
+ *  @date June 2018
+ *
+ *  @requires database
+ *  @requires logger
  */
 
 var args = [].concat(process.argv).slice(2)
@@ -24,24 +26,44 @@ const MAX_TASK_TIME = 1000 * 60 * 10 // 10 minutes
 const MIN_TASK_TIME = 1000 * 60 * 1 // 1 minute
 const NOW = Date.now()
 
-/*
- * Checks to see if a task is past its expected completion
- * time. If so, return the task, otherwise reject.
+/**
+ * The algorithm that checks if a task is old.
+ *
+ * @param {number} timing - the timing for the task it's job
+ * @param {number} mtime - of the pending task
+ * @returns {bool} - true if old, false otherwise
+ */
+var isOld = (timing, mtime) => {
+  if (timing < MIN_TASK_TIME || !timing) {
+    timing = MIN_TASK_TIME
+  } else if (timing > MAX_TASK_TIME) {
+    timing = MAX_TASK_TIME
+  }
+  return (NOW - mtime > timing)
+}
+
+/**
+ * Checks to see if a task is past its expected completion time.
+ *
+ * @param {string} taskName - the name of the task to check
+ * @returns {Promise} - resolves with the task if old, an Error otherwise
  */
 var checkIfOld = (taskName) => {
   return new Promise((resolve, reject) => {
     database.read(`tasks/pending/${taskName}`, false)
       .then(task => {
         task = task.message
-        let job = task.job
-        database.read(`jobs/open/${job}`, false)
+        database.read(`jobs/open/${task.job}`, false)
           .then(job => {
             job = job.message
-            let timing = job.exports[task.request].timing
-            if (timing < MIN_TASK_TIME || !timing) { timing = MIN_TASK_TIME } else if (timing > MAX_TASK_TIME) { timing = MAX_TASK_TIME }
+            let timing = parseFloat(job.exports[task.request].timing)
+            if (typeof timing !== 'number' || Number.isNaN(timing)) {
+              reject(new Error(`Job "${job.address}" timing is invalid for request "${task.request}"`))
+              return
+            }
             database.status(`tasks/pending/${taskName}`, false)
               .then(stats => {
-                if (NOW - stats.mtime > timing) {
+                if (isOld(timing, stats.mtime)) {
                   resolve(task)
                 } else {
                   console.log(NOW - stats.mtime, timing)
@@ -56,9 +78,11 @@ var checkIfOld = (taskName) => {
   })
 }
 
-/*
+/**
  * Gets a list of pending tasks that are past thier
  * expected completion time.
+ *
+ * @returns {Promise} - resolves a list of out of date classes
  */
 var getOldTasks = () => {
   return new Promise((resolve, reject) => {
@@ -77,13 +101,18 @@ var getOldTasks = () => {
   })
 }
 
-/*
+/**
  * Attempts to resubmit a task and put it back on the heap.
+ *
+ * @param {Object} task - the task that needs to be resubmitted
+ * @returns {Promise} - resolves true when successfuly resubmits, rejects with an Error otherwise
  */
 var resubmitTask = (task) => {
   return new Promise((resolve, reject) => {
-    if (typeof task.payment === 'undefined' || task.payment <= 0) {
+    task.payment = parseFloat(task.payment)
+    if (typeof task.payment !== 'number' || Number.isNaN(task.payment) || task.payment <= 0) {
       logger.log('No payment found on task ' + task.address)
+      reject(new Error('No payment found on task ' + task.address))
       return
     }
 
@@ -110,7 +139,7 @@ var resubmitTask = (task) => {
                 heapName += '-t-' + task.address
                 database.write(`heap/${heapName}`, '', false)
                   .then(event => {
-                    resolve(event)
+                    resolve(true)
                   })
                   .catch(onFail)
               })
@@ -122,6 +151,7 @@ var resubmitTask = (task) => {
   })
 }
 
+/** Gets old tasks and puts them back on the heap */
 var main = () => {
   console.log('main')
   getOldTasks()
