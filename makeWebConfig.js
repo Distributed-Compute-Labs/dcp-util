f6dd4544d4af83f92d33bbfe0ace568bca34851c
Wes Garland <wes@kingsds.network>
2018-11-02 16:22:15 -0400

    build system - began work on dist/-style install, fixing paths due to reorg; WIP

--- makeWebConfig.js
+++ makeWebConfig.js
@@ -16,7 +16,7 @@ require('dcp-rtlink/rtLink').link(module.paths)
 require('config').load(process.argv[2], process.argv[3])
 
 const whiteLists = {
-  default: [ 'hostname', 'port', /[a-z]U[rR][lL]$/, 'openSignup' ]
+  default: [ 'hostname', 'port', /[a-z]U[rR][lL]$/ ]
 }
 
 function safeSubset(label) {
@@ -31,22 +31,22 @@ function safeSubset(label) {
   for (p in o) {
     if (o.hasOwnProperty(p)) {
       for (let i = 0; i < whiteList.length; i++) {
-	switch (typeof whiteList[i]) {
-	case 'string':
-	  if (whiteList[i] === p) {
-	    copy[p] = o[p]
-	  }
-	case 'object':
-	  if (whiteList[i] instanceof RegExp && p.match(whiteList[i])) {
-	    copy[p] = o[p]
-	  }
-	  break
-	case 'function':
-	  if ((tmp = whiteList[i](p))) {
-	    copy[p] = tmp
-	  }
-	  break
-	}
+        switch (typeof whiteList[i]) {
+        case 'string':
+          if (whiteList[i] === p) {
+            copy[p] = o[p]
+          }
+        case 'object':
+          if (whiteList[i] instanceof RegExp && p.match(whiteList[i])) {
+            copy[p] = o[p]
+          }
+          break
+        case 'function':
+          if ((tmp = whiteList[i](p))) {
+            copy[p] = tmp
+          }
+          break
+        }
       }
     }
   }
@@ -57,9 +57,10 @@ function safeSubset(label) {
 var webConfig = {
   scheduler: safeSubset('scheduler'),
   packageManager: safeSubset('packageManager'),
-  // storage: safeSubset('storage'),
+  storage: safeSubset('storage'),
   bank: safeSubset('bank'),
   portal: safeSubset('portal'),
 }
 
-require('fs').writeFileSync(dcpConfig.root + "/www/docs/etc/dcp-config.js", 'var dcpConfig = ' + JSON.stringify(webConfig), "utf-8")
+console.log(' * Creating ' + dcpConfig.root + '/www/docs/etc/dcp-config.js')
+require('fs').writeFileSync(dcpConfig.root + '/www/docs/etc/dcp-config.js', 'var dcpConfig = ' + JSON.stringify(webConfig), 'utf-8')
