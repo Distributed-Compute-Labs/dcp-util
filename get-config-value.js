9e35b43efce391777cc1f1b3b916a16c5a0dab5b
Wes Garland <wes@kingsds.network>
2018-11-02 16:20:38 -0400

    Added get-config-value.js utility

---
+++ get-config-value.js
@@ -0,0 +1,40 @@
+#! /usr/bin/env node
+/**
+ *  @file               get-config-value.js
+ *                      Utility to query values from dcpConfig. Useful for
+ *                      shell/os-level interfaces for DCP tasks.
+ *
+ *  @author             Wes Garland, wgarland@kingsds.network
+ *  @date               Nov 2018
+ */
+require('dcp-rtlink/rtLink').link(module.paths)
+const dcpConfig = require('config').load()
+const path = require('path')
+const process = require('process')
+
+function usage() {
+  var progName = path.basename(process.argv[1])
+  
+  console.log(`
+${progName} - Query values from dcpConfig
+Copyright (c) 2018 Kings Distributed Systems Ltd., All Rights Reserved.
+
+Usage:   ${progName} path.to.config.variable [path.to.config.variable...]
+Example: ${progName} scheduler.hostname
+`)
+  process.exit(1)
+}
+
+if (process.argv.length < 3) {
+  usage()
+}
+
+
+for (let i=2; i < process.argv.length; i++) {
+  let entries = process.argv[i].split('.')
+  let entry
+  for (entry = dcpConfig; entries.length; entry = entry[entries.shift()]){
+    debugger
+  }
+  console.log(entry)
+}
