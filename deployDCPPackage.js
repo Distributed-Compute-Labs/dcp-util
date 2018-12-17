558bdf828fd968865634e0676ffa8ab88e0e6205
matthew <mpalma@sparc.network>
2018-12-17 13:45:58 -0500

    [portal, deployPackage] fixed some polyfill for module.exports at bottom of protocol and compute. blockchain-gateway now uses url endpoint before hostname:port. Deploy package now uses config for protocol.

--- deployDCPPackage.js
+++ deployDCPPackage.js
@@ -105,7 +105,7 @@ var main = async () => {
     process.exit()
   }
 
-  let baseURL = `http://${dcpConfig.packageManager.hostname}:${dcpConfig.packageManager.port}`
+  let baseURL = `${dcpConfig.packageManager.protocol || 'http:'}//${dcpConfig.packageManager.hostname}:${dcpConfig.packageManager.port}`
   let URL = baseURL + '/deploy/module'
 
   let password = await ask('Keystore password:', '', 'keypass')
