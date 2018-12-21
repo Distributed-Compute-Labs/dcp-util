43da6947d5552dbf01acb2c1d91a22bbd2da55d5
eroosenmaallen <eddie@kingsds.network>
2018-12-21 10:43:16 -0500

    Fixed documentation

--- send-template-email.js
+++ send-template-email.js
@@ -12,14 +12,14 @@ const dcpConfig = require('config').load()
 const path = require('path')
 const process = require('process')
 
-function usage() {
+function usage () {
   var progName = path.basename(process.argv[1])
-  
+
   console.log(`
 ${progName} - Send template email
 Copyright (c) 2018 Kings Distributed Systems Ltd., All Rights Reserved.
 
-Usage:   ${progName} [switches] templateFile <fromEmail> <fromName> <to> <subject> [extra]
+Usage:   ${progName} [switches] templateFile <fromEmail> <fromName> <to> <toName> <subject> [extra]
          (see: populateEmailTemplate in email.js)
 Switches:
   --stdout     - send output to stdout instead of configured mailer
@@ -28,6 +28,7 @@ Arguments:
   fromEmail    - the originator's e-mail address
   fromName     - the originator's name
   to           - the recipient's e-mail address
+  toName       - the recipient's name
   subject      - the subject of the e-mail
   extra        - JSON defining extra tokens
 `)
@@ -44,13 +45,14 @@ for (let i = 0; i < argv.length; i++) {
   }
 }
 
-if (argv < 6) {
+if (argv.length < 6) {
   usage()
 }
 
 if (argv[6]) {
   argv[6] = JSON.parse(argv[6])
 }
+
 let rfc822Message = require('email').populateEmailTemplate.apply(null, argv)
 
 if (stdout) {
