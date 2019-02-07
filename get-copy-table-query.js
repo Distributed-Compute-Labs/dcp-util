0a6864244b02661a86291d8482435977f7ec01b6
matthew <mpalma@sparc.network>
2019-02-07 16:31:40 -0500

    ormdb/mysql-iface.js - added function that returns a mysql statement to copy one table into another. Built a tool around it (bin/get-copy-table-query.js).

---
+++ get-copy-table-query.js
@@ -0,0 +1,56 @@
+#! /usr/bin/env node
+/**
+ *  @file               get-copy-table-query.js
+ *                      Get a mysql statement to copy one table into another.
+ *
+ *  @author             Matthew Palma, mpalma@kingsds.network
+ *  @date               Jan 2019
+ */
+
+/* globals dcpConfig */
+
+require('dcp-rtlink/rtLink').link(module.paths)
+require('config').load()
+const yargs = require('yargs')
+const pormdb = require('pormdb')
+
+const schemaName = yargs.argv._[0]
+const tableName = yargs.argv._[1]
+const toTableName = yargs.argv._[2]
+const ingoredColumns = yargs.argv._.slice(3)
+
+function usage () {
+  var progName = require('path').basename(process.argv[1])
+
+  console.log(`
+${progName} - Get a mysql statement to copy one table into another.
+Copyright (c) 2018 Kings Distributed Systems Ltd., All Rights Reserved.
+
+Usage:   ${progName} <schema name> <from table> <to table> [ignored columns]
+Example: ${progName} scheduler heap heap_new id
+`)
+  process.exit(1)
+}
+
+async function start () {
+  if (yargs.argv.h) {
+    usage()
+  }
+
+  if (typeof dcpConfig[schemaName] === 'undefined' || typeof dcpConfig[schemaName].ormdb === 'undefined') {
+    console.error(`No schema named: '${schemaName}' exists.`)
+    return
+  }
+
+  let db = await pormdb.createConnection(dcpConfig[schemaName].ormdb)
+  try {
+    let query = db.connection.dbConn.getCopyTableQuery(tableName, toTableName, ingoredColumns)
+    console.log(query)
+  } catch (error) {
+    console.error(error)
+  }
+
+  db.disconnect()
+}
+
+start()
