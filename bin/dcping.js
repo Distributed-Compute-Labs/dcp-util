74589f9c481435da769cf1b28ba7da44bea73ce0
eroosenmaallen <eddie@kingsds.network>
2019-07-22 10:16:00 -0400

    Merge branch 'master' of gitlab.com:Distributed-Compute-Protocol/dcp

--- bin/dcping.js
+++ bin/dcping.js
@@ -256,12 +256,19 @@ async function loadCompute () {
       process.exit(1)
     }
   }
-  
-  // Load the keystore:
-  const keystore = JSON.parse(fs.readFileSync(keyStorePath, 'ascii'))
-  const keystorePassword = await pprompt("Enter keystore password:", {method: 'hide', required: false })
 
-  protocol.keychain.addKeystore(keystore, keystorePassword, true)
+  try {
+    // Load the keystore:
+    const keystore = JSON.parse(fs.readFileSync(keyStorePath, 'ascii'))
+    const keystorePassword = await pprompt('Enter keystore password:', {
+      method: 'hide',
+      required: false
+    })
+    protocol.keychain.addKeystore(keystore, keystorePassword, true)
+  } catch (error) {
+    console.error('Unable to unlock keystore:', error.message)
+    process.exit(1)
+  }
 }
 
 /** called when a job is accepted by the scheduler
