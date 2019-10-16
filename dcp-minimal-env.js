abd327d329835ba307b885f79122cf610815280c
Wes Garland <wes@kingsds.network>
2019-10-16 11:17:20 -0400

    Renamed keychain.addKeystore to keychain.addPrivate_fromEthV3Wallet

--- dcp-minimal-env.js
+++ dcp-minimal-env.js
@@ -28,7 +28,7 @@ require('dcp-client/dist/compute.min.js')
 // const ADDRESS = '0xf56ffa118fc2f8902d30857db1eeb227753f586f'
 const KEYSTORE = { 'version': 3, 'id': '3706df1d-72b8-4f84-ab95-dfa541f2e069', 'address': 'ee99b19c0e4636499db6e502c9486189777fbc30', 'crypto': { 'ciphertext': '9ff5b2004c6f3644a336218b0fcd4015c9a61024b7cfb466bf00877ae0e46d59', 'cipherparams': { 'iv': 'd20c6003cb07ca89eef22605c2834e9e' }, 'cipher': 'aes-128-ctr', 'kdf': 'scrypt', 'kdfparams': { 'dklen': 32, 'salt': '8c6410c2a0f2271c7a74b75efbb93f973cf6e94110e40a7b6854aa907f889a60', 'n': 1024, 'r': 8, 'p': 1 }, 'mac': '884a5dfe9f4414b2ec2539cd74422b1239cf9b86f09477d7cf52bbe54a0b785e' }, 'label': 'default', 'lastActive': true, 'flags': 0 }
 const KEYSTORE_PWD = ''
-protocol.keychain.addKeystore(KEYSTORE, KEYSTORE_PWD, true)
+protocol.keychain.addPrivateKey_fromEthV3Keystore(KEYSTORE, KEYSTORE_PWD, true)
 // var paymentWallet = protocol.keychain.keys['0e57be8a17277bfb669d253c74cf89be1900c72b'].wallet
 const paymentWallet = protocol.keychain.keys['ee99b19c0e4636499db6e502c9486189777fbc30'].wallet
 
