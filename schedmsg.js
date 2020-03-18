c67b59e310564d1693960499d314fd6362394f2c
Ryan Rossiter <ryan@kingsds.network>
2020-03-18 15:11:34 -0400

    Implement SchedMsg with v4 Events

--- schedmsg.js
+++ schedmsg.js
@@ -7,147 +7,84 @@
  * schedmsg.js will convert info from command line arguments to objects. The
  * objects will be sent to protocol.js to sign and send the message
  * 
- * @author Sam Cantor, samcantor@kingsds.network
-           Duncan Mays, duncan@kingds.network
+ * @author Ryan Rossiter, ryan@kingsds.network,
+ *         Sam Cantor, samcantor@kingsds.network
+ *         Duncan Mays, duncan@kingds.network
  * @date May 2019
  */
 
-require('dcp-rtlink/rtLink').init();
+const process = require('process');
 
-const rpn = require('request-promise-native');
-const path = require('path')
-const process = require('process')
+require('yargs')
+.usage(`$0 - Send control messages to a DCP service
+Copyright (c) 2020 Kings Distributed Systems Ltd., All Rights Reserved.`)
+.command('announce <message>',
+  'Send an announcement to workers, will display a modal or log to console', {},
+  (options) => sendMessage(options, 'announce', { message: options.message }))
+.command('remove <jobAddress>', 
+  'Removes any active slices for job address', {},
+  (options) => sendMessage(options, 'remove', { jobAddress: options.jobAddress }))
+.command('openPopup <href>',
+  'Opens a new tab on browser workers', {},
+  (options) => sendMessage(options, 'openPopup', { href: options.href }))
+.command('reload',
+  'Forces a reload of the worker', {},
+  (options) => sendMessage(options, 'reload'))
+.command('restart',
+  'Stop and refreshes workers without reloading the entire worker', {},
+  (options) => sendMessage(options, 'restart'))
+.options({
+  broadcast: {
+    describe: 'when true, command will be sent to all workers connected to the scheduler (provided keystore must be in the scheduler\'s schedMsgAdmins list). When false, command will be sent to workers that use the provided keystore as their identity',
+    type: 'boolean',
+    default: false,
+  },
+  persistent: {
+    describe: '(not implemented) Whether the message should be persistent',
+    type: 'boolean',
+    default: false,
+  },
+  keystore: {
+    describe: 'Path to the keystore to use for authorization',
+    type: 'string',
+  },
+  scheduler: {
+    describe: 'URL of the scheduler to send the command to',
+    type: 'string',
+    default: 'https://scheduler.distributed.computer/',
+  }
+})
+.demandCommand(1)
+.strict().argv;
 
-const options = require('yargs')
-  .usage(`$0 - Send control messages to a DCP service
-Copyright (c) 2019 Kings Distributed Systems Ltd., All Rights Reserved.
 
-$0 --type [message type] --payload [message content]
-
-Examples: $0 --type 'broadcast' --payload 'Hello World!' --persistent
-          $0 --type 'command' --payload 'reload'
-          $0 --type 'command' --payload 'remove,0x12345'`)
-  .describe('type', 'type of message being send (broadcast, command, delete)')
-  .describe('payload', 'message payload to send (eg. broadcast content)')
-  .describe('persistent', 'whether the message should be persistent')
-  .boolean('persistent')
-  .default('persistent', false)
-  .describe('keystore', 'path to the keystore to use')
-  .describe('scheduler', 'Specify an alternate scheduler')
-  .default('scheduler', 'https://scheduler.distributed.computer/')
-  .demandOption(['type','payload'])
-  .hide('version')
-  .epilogue(`
-Options for --type 'command':
-  --payload 'popupMessage, [url]'     opens a new tab with given url
-            'reload'                  kills and reloads workers 
-            'restart'                 stop and refreshes workers without reloading the entire worker
-            'remove,[job id]'   removes any active tasks that contain a given job id`)
-  .argv;
-
-
-/** Send the message to the scheduler
- *  @param      msg             Message to send
- *              .type           Message type: command, broadcast, etc.
- *              .payload        Content is specific to message type
+/**
+ * Send the message to the scheduler
+ * @param {object} options - yarg options
+ * @param {string} command
+ * @param {object} payload
  */
-async function sendMessage (msg) {
-  //adds a timestamp to the msg, all the other information and formatting is already complete
-  msg.timestamp = Date.now()
+async function sendMessage (options, command, payload={}) {
+  await require('dcp-client').init(options.scheduler);
+  const wallet = require('dcp/wallet');
+  const protocolV4 = require('dcp/protocol-v4');
+  const { URL } = require('dcp/dcp-url');
 
-  let result
+  const schedulerURL = new URL(options.scheduler);
+  const schedulerConnection = new protocolV4.Connection(schedulerURL.resolve('/DCPv4'));
+  const keystore = await wallet.get(options.keystore);
 
-  try {
-    const wallet = await require('dcp/wallet').get(options.keystore);
-    
-    const scheduler = options.scheduler 
-      ? new (require('dcp/dcp-url').URL)(options.scheduler) 
-      : require('dcp/dcp-config').scheduler.location;
-
-    //calls the needed route
-    console.log('x1: sending...', scheduler.toString(), msg);
-    result = await require('dcp/protocol').send(scheduler.resolve('msg/send'), msg, wallet);
-  } catch (error) {
-    //logs the error
-    console.error('send failed', error)
-    result = error
-  }
-  //disconnects from protocol
-  require('dcp/protocol').disconnect()
-
-  //returns the error for analysis, returns null if no error occured
-  return result
-}
-
-async function start () {
-  //the message sent to the scheduler will be configuired by the caller of this program using CLI arguements
-
-  const msg = {}
-
-  msg.type = options['type']
-  msg.persistent = options['persistent']
-  //mimic worker objects will be true so that they can skip steps
-  msg.mimic = false
-
-  switch (msg.type) {
-    case 'command':
-      let payload = options['payload']
-      let a = payload.split(',')
-      switch (a[0]){
-        case 'popupMessage':
-          msg.payload = {
-            command: 'openPopup',
-            href: a[1]
-          }
-          break
-        case 'reload':
-          msg.payload = {
-            command: 'reload',
-            perform: true
-          }
-          break
-        case 'restart':
-          msg.payload = {
-            command: 'restart',
-            perform: true
-          }
-          break
-        case 'remove':
-          msg.payload = {
-            command: 'remove',
-            perform: true,
-            jobId : a[1]
-          }
-          break
-        default:
-          console.log(`Error: ${a[0]} is not a valid message payload`)
-          help()
-          process.exit(1)
-      }
-      msg.persistent = false
-      break
-
-    case 'broadcast':
-      msg.payload = options['payload']
-      break
-
-    case 'delete':
-      msg.payload = 'delete'
-      break
-    default:
-      console.log(`Error: ${msg.type} is not a valid message type`)
-      help()
-      process.exit(1)
-      
+  const message = {
+    owner: keystore.address,
+    broadcast: options.broadcast,
+    command,
+    payload,
   }
 
-  const result = await sendMessage(msg)
-  
-  console.log('Result:', result)
+  console.log(`Sending SchedMsg to scheduler ${options.scheduler}:`, message);
+  await schedulerConnection.send('sendSchedMsg', message, keystore);
+  console.log(`\nCommand '${command}' sent successfully.`);
 
-  //exits the program without error flag
-  process.exit(0)
+  await schedulerConnection.close();
+  process.exit(0);
 }
-
-require('dcp-client').init(options.scheduler || undefined)
-.then(() => start());
